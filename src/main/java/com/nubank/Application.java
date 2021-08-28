ffb073cad77dad9dd368669719407a3915c7dc4e
Thiago José da Silva <tjs.thiago@gmail.com>
2021-08-27 23:07:10 -0300

    initial commit

---
+++ src/main/java/com/nubank/Application.java
@@ -0,0 +1,9 @@
+package com.nubank;
+
+public class Application {
+	public static void main(String[] args) {
+		for (String arg : args) {
+			System.out.println(arg);
+		}
+	}
+}
---
+++ src/test/java/com/nubank/ApplicationTest.java
@@ -0,0 +1,5 @@
+package com.nubank;
+
+public class ApplicationTest {
+
+}
